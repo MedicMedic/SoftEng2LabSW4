d3c9c0e3a3699b366eaa6e7918fe994fd74ebf86
Marianne Edic <142379309+MedicMedic@users.noreply.github.com>
2024-04-18 04:28:15 +0000

    ugh

--- UnliCallTextPackage.java
+++ UnliCallTextPackage.java
@@ -3,31 +3,17 @@ import java.util.Map;
 
 public class UnliCallTextPackage implements UnliCallsTextOffer
 {
-    private static Map<String, Boolean> usageMap = new HashMap<>();
+    private static Map<String, String> offerMap = new HashMap<>();
     
     static{
-        usageMap.put("Smart", false);
-        usageMap.put("Globe", true);
-        usageMap.put("Ditto", true);
+        offerMap.put("Smart", "Does not offer any free calls or texts, and you will be charged per use.");
+        offerMap.put("Globe", "Comes with unlimited calls and texts to subscribers within their network. Calls and texts to other networks are charged extra.");
+        offerMap.put("Ditto", "Includes unlimited calls and texts to all networks within the country.");
     }
     
     @Override
     public String showUnliCallsTextOffer(String telcoName, boolean unliCallText)
     {
-        String offersUnli;
-
-        if (telcoName == "Smart")
-        {
-            offersUnli = "Does not offer any free calls or texts, and you will be charged per use.";
-        }
-        else if (telcoName == "Globe")
-        {
-            offersUnli = "Comes with unlimited calls and texts to subscribers within their network. Calls and texts to other networks are charged extra.";
-        }
-        else
-        {
-            offersUnli = "Includes unlimited calls and texts to all networks within the country.";
-        }
-        return offersUnli;
+        return offerMap.get(telcoName);
     }
 }
